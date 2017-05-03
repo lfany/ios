b73921eaca665aafd191b8d433eb1723016a7484
hello <hello@ifnot.cc>
2017-05-03 22:10:18 +0800

    initial commit

---
+++ oc/oc/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  oc
+//
+//  Created by ifnot on 2017/4/27.
+//  Copyright © 2017年 ifnot.me. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+// 定义main方法，作为程序入口
+int main(int argc, char *argv[])
+{
+    @autoreleasepool
+    {
+        // 执行输出
+        NSLog(@"Hello Objective-C");
+    }
+    // 返回结果
+    return 0;
+}
