96ac9fc141700097443cdc1d79d5c5b16ce9ee73
hello <hello@ifnot.cc>
2017-05-03 22:22:08 +0800

    backgroundColor NSLog

--- FirstXcodeProj/FirstXcodeProj/ViewController.m
+++ FirstXcodeProj/FirstXcodeProj/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"Log Log");
+    self.view.backgroundColor = [UIColor redColor];
 }
 
 
