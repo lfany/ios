fca5638f2f1f9f9c619ca4b79ab047825ad6e6f9
hello <hello@ifnot.cc>
2017-05-04 23:14:13 +0800

    appcode hello proj

---
+++ hello/hello/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hello
+//
+//  Created by ifnot on 2017/5/4.
+//  Copyright (c) 2017 ifnot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
---
+++ hello/hello/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hello
+//
+//  Created by ifnot on 2017/5/4.
+//  Copyright (c) 2017 ifnot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ViewController : UIViewController
+
+
+@end
