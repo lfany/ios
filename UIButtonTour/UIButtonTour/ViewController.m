e0c118ee8d8b8cd1d394b36141d7b5edbe4b0598
hello <hello@ifnot.cc>
2017-05-04 22:52:14 +0800

    btn

--- UIButtonTour/UIButtonTour/ViewController.m
+++ UIButtonTour/UIButtonTour/ViewController.m
@@ -14,9 +14,31 @@
 
 @implementation ViewController
 
+- (void)createBtn {
+    UIButton* btn = [UIButton buttonWithType:UIButtonTypeSystem];
+    
+    btn.frame = CGRectMake(100, 100, 100, 40);
+    
+    [btn setTitle:@"xxx" forState:UIControlStateNormal];
+    [btn setTitle:@"yyy" forState:UIControlStateHighlighted];
+    
+//    btn.font = [UIFont systemFontOfSize:18];
+//    [btn.titleLabel setFrame:CGRectMake(0, 0, 100, 100)];
+//    btn.titleLabel.hidden = NO;
+//    btn.titleLabel.text = @"xxxx";
+    btn.titleLabel.textColor = [UIColor orangeColor];
+    
+    btn.titleLabel.backgroundColor = [UIColor greenColor];
+    
+    btn.backgroundColor = [UIColor redColor];
+    
+    [self.view addSubview:btn];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self createBtn];
 }
 
 
