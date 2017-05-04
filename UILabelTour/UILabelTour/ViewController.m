08f67e335ccdfa1b5a95f3b3d785e76532bc7678
hello <hello@ifnot.cc>
2017-05-04 21:55:38 +0800

    tour

--- UILabelTour/UILabelTour/ViewController.m
+++ UILabelTour/UILabelTour/ViewController.m
@@ -14,9 +14,37 @@
 
 @implementation ViewController
 
+- (void)createUI {
+    UILabel* label = [[UILabel alloc] init];
+    
+    label.text = @"你好, 世界!abcdefghijk \
+    你好, 世界!abcdefghijk \
+    你好, 世界!abcdefghijk";
+    
+    label.backgroundColor = [UIColor clearColor];
+    
+    label.frame = CGRectMake(100, 100, 200, 200);
+    
+    [self.view addSubview:label];
+    
+    label.textColor = [UIColor grayColor];
+    
+    label.font = [UIFont systemFontOfSize:22];
+    
+    label.shadowColor = [UIColor blueColor];
+    
+    label.shadowOffset = CGSizeMake(3, 3);
+    
+    label.numberOfLines = 0;
+    
+    label.textAlignment = NSTextAlignmentCenter;
+    
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self createUI];
 }
 
 
