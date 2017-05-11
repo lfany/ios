725eb1bbb58cb7b18b7c48fb678c1f6da511b66e
hello <hello@ifnot.cc>
2017-05-11 22:11:56 +0800

    update

--- UIButtonClickTour/UIButtonClickTour/ViewController.m
+++ UIButtonClickTour/UIButtonClickTour/ViewController.m
@@ -18,22 +18,40 @@
 
     UIButton* btn = [UIButton buttonWithType:UIButtonTypeSystem];
     btn.frame = CGRectMake(100, 100, 80, 40);
+    btn.tag = 101;
     
     [btn setTitle:@"按钮" forState:UIControlStateNormal];
 
-    [btn addTarget:self action:@selector(pressBtn) forControlEvents:UIControlEventTouchUpInside];
+    [btn addTarget:self action:@selector(pressBtn:) forControlEvents:UIControlEventTouchUpInside];
     
     [self.view addSubview:btn];
 }
 
-- (void)pressBtn {
-    NSLog(@"按钮按下了...");
+- (void)createBtn2 {
+    UIButton* btn = [UIButton buttonWithType:UIButtonTypeSystem];
+    btn.frame = CGRectMake(100, 200, 80, 40);
+    btn.tag = 102;
+    
+    [btn setTitle:@"xxx" forState:UIControlStateNormal];
+    
+    [btn addTarget:self action:@selector(pressBtn:) forControlEvents:UIControlEventTouchDown];
+    
+    [self.view addSubview:btn];
 }
+
+- (void)pressBtn:(UIButton*) btn {
+    if(btn.tag == 101){
+        NSLog(@"按钮按下了...");
+    }else {
+        NSLog(@"xxx");
+    }
+    }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     [self createBtn];
+    [self createBtn2];
 }
 
 
